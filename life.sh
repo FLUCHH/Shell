070fa97799b79ee32e8f65d11e736729b8f92f8e
Luca Frattini <l.frattini@student.tudelft.nl>
2022-10-27 12:46:12 +0200

    fixed bugs

--- life.sh
+++ life.sh
@@ -1,14 +1,15 @@
-#! /bin/bash
-# looks for the future in your directories
+#!/bin/bash
+# looks for the future in your directory
 
-USAGE="usage: 
+USAGE="USAGE: 
        no arguments, looks for the \"future\" word and gather those files
        -n, --new     creates a file \"future.txt\" in the current directory
                      and add some text
        -h, --help    ask for help to figure your future out"
 
+: <<'com'
 # check if there are any typos in the command       
-function syntax_check(){
+syntax_check () {
        # check for unused arguments
        if [[ "${v}" -gt 0 ]]; then
               err "Invalid syntax!"
@@ -18,13 +19,14 @@ function syntax_check(){
 }
 
 # check if everything went smoothly
-function check(){
+check () {
        if [[ $? == 1 ]]; then
               echo "ERROR: copy gone wrong"
               exit 1
        fi
 
 }
+com
 
 v=0
 while [[ $# -gt 0 ]]; do
@@ -34,13 +36,14 @@ while [[ $# -gt 0 ]]; do
        case "${arg}" in
               "-h" | "--help")
                      echo "Sorry, still figuring out"
+                     echo "${USAGE}"
                      exit 0
                      ;;
               "-n" | "--new")
-                     echo "Just some tips:
-                           new job? New style? New friends?" | cat > future.txt
+                     echo "Just some tips: new job? New style? New friends?" | cat > future.txt
+                     echo "Current state: "
+                     cat future.txt
                      echo "Let's start from scratch"
-                     cd ~ || cd "$HOME" || exit 0
                      exit 0
                      shift
                      ;;
@@ -52,21 +55,29 @@ while [[ $# -gt 0 ]]; do
 done
 
 # check if there are any typos in the command       
-syntax_check
+if [[ "${v}" -gt 0 ]]; then
+              err "Invalid syntax!"
+              echo "${USAGE}"
+              exit 1 
+       fi
 
 # look for the Future
 grep -rl future "${PWD}" | cat > now.txt
 
-echo "cane"
 # did we find any future?
-if [[ -s now.txt ]]; then
-       echo "Looks like there is no future in this directory
-              consider using the \"-n\" option"
+if [[ ! -s now.txt ]]; then
+       echo "Looks like there is no future in this directory, consider using the \"-n\" option"
        exit 0
 fi
 
 # check if everything went smoothly
-check
+if [[ $? == 1 ]]; then
+              echo "ERROR: copy gone wrong"
+              exit 1
+else
+       echo "done"
+fi
+
 
 
 
